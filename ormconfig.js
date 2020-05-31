595262c7dd52fcadb29214bc2bb4916e5b37af29
Angelo Gonçalves da Luz <angelogl@gmail.com>
2020-05-31 18:47:21 -0300

    :wrench: Add heroku configurations

---
+++ ormconfig.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+  "type": "postgres",
+  "url" : process.env.DATABASE_URL,
+  "entities": [
+    "dist/models/**/*.js"
+ ],
+ "migrations": [
+  "dist/database/migrations/**/*.js"
+],
+ "cli":{
+  "migrationsDir": [
+    "src/database/migrations/"
+  ],
+  "entitiesDir": "src/models"
+  }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
+import dotenv from 'dotenv';
 import app from './app';
 import 'reflect-metadata';
 import './database';
 
-app.listen(3000, () => {
+dotenv.config();
+
+app.listen(process.env.PORT || 3000, () => {
   console.log('🏃 Running Server');
 });
