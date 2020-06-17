6864030d700a2d97de69c2625d43bf8054a5f8f1
Angelo Gonçalves da Luz <angelogl@gmail.com>
2020-06-17 18:36:07 -0300

    :bug: fix routes index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import classRouter from './class.routes';
 import lessonRouter from './lesson.routes';
+import studentRouter from './student.routes';
+import contentRouter from './content.routes';
 
 const routes = Router();
 
 routes.use('/class', classRouter);
 routes.use('/lesson', lessonRouter);
-routes.use('/student', lessonRouter);
-routes.use('/content', lessonRouter);
+routes.use('/student', studentRouter);
+routes.use('/content', contentRouter);
 
 export default routes;
