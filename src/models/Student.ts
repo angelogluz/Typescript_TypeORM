a8b10fbfa41ae113fbf69b92cd12bd7225d6b858
Angelo Gonçalves da Luz <angelogl@gmail.com>
2020-06-21 10:36:32 -0300

    :sparkles: Add class-validator dependency and example

--- src/models/Student.ts
+++ src/models/Student.ts
@@ -7,6 +7,8 @@ import {
   ManyToMany,
   JoinTable,
 } from 'typeorm';
+
+import { IsEmail, Max, Min, MaxLength, MinLength } from 'class-validator';
 import Class from './Class';
 
 @Entity('student')
@@ -15,12 +17,17 @@ export default class Student {
   id: string;
 
   @Column()
+  @MaxLength(50, { message: 'Nome precisar no máximo 50 caracteres' })
+  @MinLength(2, { message: 'Nome deve possuir no mínimo 1 caractere' })
   name: string;
 
   @Column()
+  @Max(99999, { message: 'Chave inválida' })
+  @Min(10000)
   key: number;
 
   @Column()
+  @IsEmail()
   email: string;
 
   @ManyToMany(type => Class)
--- src/routes/student.routes.ts
+++ src/routes/student.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { getRepository } from 'typeorm';
+import { validate } from 'class-validator';
 import Student from '../models/Student';
 
 const studentRouter = Router();
@@ -7,8 +8,21 @@ const studentRouter = Router();
 studentRouter.post('/', async (request, response) => {
   try {
     const repo = getRepository(Student);
-    const res = await repo.save(request.body);
-    return response.status(201).json(res);
+    const { key, name, email } = request.body;
+
+    const student = repo.create({
+      key,
+      name,
+      email,
+    });
+
+    const errors = await validate(student);
+
+    if (errors.length === 0) {
+      const res = await repo.save(student);
+      return response.status(201).json(res);
+    }
+    return response.status(400).json(errors);
   } catch (err) {
     console.log('err.message :>> ', err.message);
     return response.status(400).send();
