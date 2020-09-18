9ff1acedca1ca2407e9ac360d4bf12c859e47e78
Angelo Gonçalves da Luz <angelogl@gmail.com>
2020-09-18 17:35:18 -0300

    :whale: Add docker to project
    Docker, adjust .env and ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -1,28 +1,18 @@
-console.log('process.env.DATABASE_URL :>> ', process.env.DATABASE_URL);
-console.log('process.env.DATABASE_URL :>> ', process.env.ENTITIES);
-console.log('process.env.DATABASE_URL :>> ', process.env.MIGRATIONS);
-module.exports = {
 
-  "type": "postgres",
-  "url" : process.env.DATABASE_URL,
-  "logging": true,
-  "cache": {
-    type: "redis",
-    options: {
-      host: "localhost",
-      port: 6379
-    }
-  },
-  "entities": [
-    process.env.ENTITIES
- ],
- "migrations": [
-  process.env.MIGRATIONS
-],
- "cli":{
-  "migrationsDir": [
-    "src/database/migrations/"
-  ],
-  "entitiesDir": "src/models/"
-  }
-}
+module.exports = {
+    "type": "postgres",
+    "host": process.env.DB_HOST,
+    "port": +process.env.DB_PORT,
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "migrations": [process.env.MIGRATIONS],
+    "entities": [process.env.ENTITIES],
+    "synchronize": process.env.DB_SYNC == "true",
+    "cli":{
+      "migrationsDir": [
+      "src/database/migrations/"
+    ],
+    "entitiesDir": "src/models/"
+    },
+ }
--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,6 @@ import './database';
 
 dotenv.config();
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('🏃 Running Server');
+app.listen(process.env.API_PORT || 3000, () => {
+  console.log('🏃 Running Server on ', process.env.API_PORT);
 });
